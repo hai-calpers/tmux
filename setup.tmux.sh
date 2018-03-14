a9df7bc10512cd33b94913de43daa3fe8c70279c
Hai Nguyen <hnguyen6@lgdiansible2.calpers.ca.gov>
2018-03-14 12:10:29 -0700

    setup file added

---
+++ setup.tmux.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Check if the file exist
+
+# Create a soft link so that it would pick it up.
+if [ -e ~/.tmux/.tmux.conf ]
+then
+    # then create symlink
+    ln -s -f ~/.tmux.conf ~/.tmux/.tmux.conf
+fi
+
+# Have to set tmp directory to user profile instead of system wide /tmp
+if ! grep -Fxq "export TMUX_TMPDIR=~/.tmux/tmp" ~/.bashrc 
+then
+    # add it to the file
+    echo "export TMUX_TMPDIR=~/.tmux/tmp" >> ~/.bashrc
+fi
